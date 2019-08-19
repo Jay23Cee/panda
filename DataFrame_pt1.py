28d87cb6589333f42cb6afe9bcacf22eefa86300
Jay23Cee <41491633+Jay23Cee@users.noreply.github.com>
2019-08-19 12:26:24 -0700

    Create DataFrame_pt1.py

---
+++ DataFrame_pt1.py
@@ -0,0 +1,24 @@
+#pandas Dataframe
+import pandas as pd
+
+#df = pd.DataFrame()
+#print(df)
+
+
+## create DataFramr from list
+#data = [1,2,3,4,5]
+#df = pd.DataFrame(data)
+#print(df)
+
+			#example 2  List
+#data = [['Alex',10 ], ['Bob',12] , ['Clarke',13]]
+#df = pd.DataFrame( data, columns = [ 'Name' , 'Age'])	#DataFrame()
+#print(df)
+
+##Example 3
+#data = [['Alex',10 ], ['Bob',12] , ['Clarke',13]]
+#df = pd.DataFrame( data, columns = [ 'Name' , 'Age'], dtype = float )	#DataFrame() dtype =float
+#print(df)
+
+#CREATE A DATAFRAME FROM DICT
+#of ndarrays /list
