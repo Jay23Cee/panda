b35b4f06f6b44e84d8b8d6f082d1ef85f997b8f4
Jay23Cee <41491633+Jay23Cee@users.noreply.github.com>
2019-08-19 19:29:26 -0700

    added DataFrame
    added DataFrame from List of Dict, and ndArrays/List

--- DataFrame_pt1.py
+++ DataFrame_pt1.py
@@ -20,5 +20,57 @@ import pandas as pd
 #df = pd.DataFrame( data, columns = [ 'Name' , 'Age'], dtype = float )	#DataFrame() dtype =float
 #print(df)
 
+
+###########################
+############################
+
+
 #CREATE A DATAFRAME FROM DICT
 #of ndarrays /list
+#data = { 'Name': ['Tom', 'Jack', 'Steve', 'Ricky'], 'Age': [28,34,29,42]}
+
+#df = pd.DataFrame(data, index=['rank1', 'rank2', 'rank3', 'rank4'])
+#print(df)
+
+
+#Example2
+#data ={'Name':['Tom', 'Jack', 'Steve', 'Ricky'], 'Age': [28,34,29,42]}
+#df = pd.DataFrame(data, index= ['rank1', 'rank2', 'rank3', 'rank4'])
+#print(df)
+
+##############################
+##############################
+
+#Create a DataFrame from List of Dicts
+#Example 1
+        ##The following  example showshowto createa DataFrame by passing a list of dictionaries
+#data = [{'a': 1, 'b': 2}, {'a':5 , 'b': 10, 'c':20}]
+#df = pd.DataFrame(data)
+#print(df)
+
+
+#example 2
+#data = [{'a':1, 'b':2}, {'a': 5, 'b':10 , 'c':20}]
+#df = pd.DataFrame(data, index = ['first', 'second'])
+#print(df)
+
+
+#example 3
+#data = [{'a': 1, 'b':2}, {'a':5, 'b':10, 'c': 20}]
+
+            #with2 ccolumn indices, values same as dictionary keys 
+#df1 = pd.DataFrame(data, index= ['first', 'second'], columns= ['a', 'b'])
+            #with 2 column indices with one index with other 
+#df2 = pd.DataFrame(data, index = ['first','second'], columns=['a', 'b1'])
+
+#print(df1)
+#print("\n")
+#print(df2)
+
+
+##create a DataFrame from Dict of Series
+
+
+
+
+
